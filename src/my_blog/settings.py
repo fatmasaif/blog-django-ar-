b4bf9a594b5bdb62493e6a4be8880c85ded086fd
fatmasaif <laidanifatma@gmail.com>
2022-10-23 22:30:48 +0100

    navbar and dropdown

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -118,10 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static'),
-    '/home/droit/Documents/MyProjects/Blogger/src/my_blog/static',
+    '/home/droit/Documents/MyProjects/Blogger/src/static',
+    
 ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
