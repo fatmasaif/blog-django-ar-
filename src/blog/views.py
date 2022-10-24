aff5f969ca2b702e6a1c24f5f24959ff2da40d1a
fatmasaif <laidanifatma@gmail.com>
2022-10-24 10:03:35 +0100

    List Posts manul exemple

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
+posts =[
+    {
+        'title' :'التدوينة 1',
+        'content': '  1نص التدوينة',
+        'post_date': '15-3-2019',
+        'auther' : '1فاطمة',
+    },
+    {
+        'title' :'2 التدوينة',
+        'content': ' 2نص التدوينة',
+        'post_date': '15-3-2019',
+        'auther' : '2فاطمة',
+    },
+    {
+        'title' :'التدوينة 3',
+        'content': ' 3نص التدوينة',
+        'post_date': '15-3-2019',
+        'auther' : ' 3فاطمة',
+    },
+
+]
+
 def home(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'title' : 'الصفحة الرئيسية',
+        'posts'  : posts,
+    }
+    return render(request, 'blog/index.html',context)
