ca4d8470cba225de68169440911520e87f974d59
fatmasaif <laidanifatma@gmail.com>
2022-10-28 13:50:39 +0100

    ADD Comment with forms

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,35 @@
+from email.policy import default
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title      = models.CharField(max_length = 100)
+    content    = models.TextField()
+    post_date  = models.DateTimeField(default=timezone.now)
+    post_update= models.DateTimeField(auto_now = True)
+    author     = models.ForeignKey(User ,on_delete =models.CASCADE)
+   
+      #fontion for display title in adminPanel
+    def __str__(self):
+        return self.title
+
+    class Meta :
+        ordering = ('-post_date',)   
+
+
+
+class Comment(models.Model) :
+  name         = models.CharField( max_length = 50,  verbose_name= 'الاسم')   
+  email        = models.EmailField(verbose_name= 'الايميل')
+  body         = models.TextField(verbose_name= 'التعليق')
+  comment_date = models.DateTimeField(auto_now_add =True)
+  active       = models.BooleanField(default = False)
+  post         = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')   
+ 
+  def __str__ (self):
+    return 'علق {} على {}.'.format(self.name,self.post)
+
+  class Meta:
+    ordering =('-comment_date',)  
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,31 +1,12 @@
-from django.shortcuts import render
-
-posts =[
-    {
-        'title' :'التدوينة 1',
-        'content': '  1نص التدوينة',
-        'post_date': '15-3-2019',
-        'auther' : '1فاطمة',
-    },
-    {
-        'title' :'2 التدوينة',
-        'content': ' 2نص التدوينة',
-        'post_date': '15-3-2019',
-        'auther' : '2فاطمة',
-    },
-    {
-        'title' :'التدوينة 3',
-        'content': ' 3نص التدوينة',
-        'post_date': '15-3-2019',
-        'auther' : ' 3فاطمة',
-    },
-
-]
+from re import I
+from django.shortcuts import render, get_object_or_404
+from . models import Post
+from . forms import NewComment
 
 def home(request):
     context = {
         'title' : 'الصفحة الرئيسية',
-        'posts'  : posts,
+        'posts'  : Post.objects.all(),
     }
     return render(request, 'blog/index.html',context)
 
@@ -37,3 +18,16 @@ def about(request):
       
       }
     return render(request, 'blog/about.html',context)    
+
+def post_detail(request, post_id):
+    post     = get_object_or_404(Post, pk=post_id)
+    comments = post.comment.filter(active =True)
+    # comments معناها جيب جميع التعليقات المتعلقة بالمدونة 
+    comment_form = NewComment()
+    context  ={
+        'title' : 'تفاصيل التدوينة',
+        'post'  :  post,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+    return render (request, 'blog/post_detail.html',context)    
