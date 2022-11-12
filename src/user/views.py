036986c70dafdc45fdc59773b4b10407c3ad25a6
fatmasaif <laidanifatma@gmail.com>
2022-11-12 07:42:03 +0100

    ADD register user

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+def register(request):
+    if request.method =='POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'تهانينا {username} لقد تمت عملية التسجيل بنجاح')
+            return redirect('home')
+
+    else:
+         form = UserCreationForm()    
+    context = {
+        'title': 'التسجيل',
+        'form' : form
+    }
+    return render (request, 'user/register.html',context)
